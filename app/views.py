8c00cedec0422de241691687c51648d850db589f
Tiffany <tiffany.c.stallings@gmail.com>
2017-10-22 20:21:54 -0400

    Add routes for core pages

--- app/views.py
+++ app/views.py
@@ -10,6 +10,27 @@ app = Flask(__name__)
 def showLanding():
 	return 'This is the landing page!'
 
+
+@app.route('/login')
+def showLogin():
+	return 'This is the login page!'
+
+
+@app.route('/adoptions')
+def showAdoptions():
+	return 'This is the adoptions page!'
+
+
+@app.route('/<int:user_id>/profile')
+def showProfile(user_id):
+	return 'This is the profile for user # ' + str(user_id)
+
+
+@app.route('/<int:user_id>/pets')
+def showPets(user_id):
+	return 'This is the pets page for user # ' + str(user_id)
+
+
 if __name__ == '__main__':
 	app.secret_key = 'some_secret_key'
 	port = int(os.environ.get('PORT', 5000))
