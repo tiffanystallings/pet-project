6ed2f03cc208d1e9404bbe4aed79dbbd68955845
Tiffany <tiffany.c.stallings@gmail.com>
2017-11-06 13:32:40 -0500

    Draw egg on canvas

--- app/static/engine.js
+++ app/static/engine.js
@@ -2,9 +2,20 @@ var Engine = (function(global) {
 	var doc = global.document,
 		win = global.window,
 		canvas = doc.createElement('canvas'),
-		ctx = canvas.getContext('2d');
+		ctx = canvas.getContext('2d'),
+		egg = new Image();
+	egg.src = '/static/images/egg_ph.png';
 
 	canvas.width = 300;
 	canvas.height = 300;
 	$('#canvas').append(canvas);
+
+	function init() {
+		ctx.clearRect(0, 0, 300, 300);
+		ctx.drawImage(egg, 0, 0);
+	}
+
+	egg.onload = function() {
+		init();
+	}
 })(this);
